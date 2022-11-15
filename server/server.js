b131a6a4f1303115571ced1410baaa1257ef4363
Andrew Vijandre <vijandreandrew@yahoo.com>
2022-11-15 14:51:42 -0500

    config and server connecting

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const DB = 'Authors'
+const DB = 'aura'
 
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-require('./config/mongoose.config')(DB)
-require('./routes/author.routes')(app);
+require('./config/config')(DB)
+// require('./routes/author.routes')(app);
 
 
 app.listen(8000, () => console.log("Listening at Port 8000"))
