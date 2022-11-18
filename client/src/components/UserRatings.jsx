788a2d4882aacd3cfdfb881b1135d40c9545330c
brobichaux7 <robichauxbrock4@gmail.com>
2022-11-18 11:46:05 -0600

    created beginning of user profile

--- client/src/components/UserRatings.jsx
+++ client/src/components/UserRatings.jsx
@@ -1,18 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios';
-import GuestNavBar from './GuestNavBar';
 import UserNavBar from './UserNavBar';
-import { useNavigate, Link } from 'react-router-dom';
+import GuestNavBar from './GuestNavBar';
 import musicStyle from './Home.module.css'
 
-const UserProfile = () => {
+const UserRatings = () => {
   
   // user variable
   const [user, setUser] = useState({});
   const [loggedIn, setLoggedIn] = useState(false);
-
-  // redirect
-  const navigate = useNavigate();
   
   useEffect(()=>{
     axios.get("http://localhost:8000/api/users/checkUser", {withCredentials:true})
@@ -31,17 +27,16 @@ const UserProfile = () => {
         })
 }, [])
 
-
-  return (
+    return (
     <div>
-      {
-        loggedIn ? <UserNavBar /> : <GuestNavBar />
-      }
-      <Link to={`/musicList/${user._id}`}>
-        <h1>Ratings</h1>
-        </Link>
+        {
+            loggedIn ? <UserNavBar /> : <GuestNavBar />
+        }
+        <div>
+            <h1 className={musicStyle.searchArtist}>{user.name}'s Music Taste</h1>
+        </div>
     </div>
   )
 }
 
-export default UserProfile
+export default UserRatings
