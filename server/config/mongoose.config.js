f5afe88ab42952a7ca3062ec2bf4dc86c6c2dbe2
brobichaux7 <robichauxbrock4@gmail.com>
2022-11-18 02:50:43 -0600

    finished login and reg in the backend

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = (DB) => {
 
-    mongoose.connect('mongodb://localhost/' + DB)
+    mongoose.connect('mongodb://127.0.0.1/' + DB)
         .then(() => console.log(`Established a connection to the ${DB} database`))
         .catch(err => console.log(`Something went wrong when connecting to ${DB} `, err));
 }
